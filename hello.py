e2fca9e0ec3272e03b75384de77cc913b3b2b9e3
Gabriel Carlos <gabrielcarlosoliveira0@gmail.com>
2025-09-29 21:10:31 -0300

    New project test

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+usuarios = []
+
+@app.route("/health")
+def health():
+    return { "status": "ok" }, 200
+
+@app.route("/user", methods=["POST"])
+def create_user():
+        data = request.get_json()
+        nome = data.get("nome")
+
+        if not nome:
+            return jsonify({ "error": "Nome is required" }), 400
+        
+        usuarios.append(nome)
+        return jsonify ({
+            "message": "Usuario Adicionado",
+            "usuarios": usuarios
+            }), 201
+    
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify({"usuarios": usuarios}), 200
+
---
+++ test_requests.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+
+url = "http://127.0.0.1:5000"
+
+def test_health_check():
+    response = requests.get(f"{url}/health")
+    print (response.text)
+    assert response.status_code == 200
+
+def test_get_users():
+    response = requests.get(f"{url}/users")
+    print (response.text)
+    assert response.status_code == 200
+
+def test_post_user():
+    payload = {"nome": "Teste"}
+    response = requests.post(f"{url}/user", json=payload)
+    print (response.text)
+    assert response.status_code == 201
