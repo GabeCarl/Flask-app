15c546accc35064ca2af8b0d515c7a48d5e788a7
Gabriel Carlos <gabrielcarlosoliveira0@gmail.com>
2025-10-29 07:28:05 -0300

    delete pytest in dockerfile and requirements, execution only in pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:latest
+FROM python:3.12-alpine
 WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-COPY test_requests.py hello.py ./
+COPY hello.py .
 EXPOSE 5000
 CMD ["flask", "--app", "hello", "run", "--host=0.0.0.0", "--port=5000"]
--- test_requests.py
+++ test_requests.py
@@ -1,20 +1,24 @@
+from hello import app 
+# Import pytest for writing and running tests
 import pytest
-import requests
 
-url = "http://127.0.0.1:5000"
+@pytest.fixture
+def client():
+    """A test client for the app."""
+    with app.test_client() as client:
+        yield client
 
-def test_health_check():
-    response = requests.get(f"{url}/health")
-    print (response.text)
+def test_health_check(client):
+    """Test the health check route."""
+    response = client.get('/health')
     assert response.status_code == 200
 
-def test_get_users():
-    response = requests.get(f"{url}/users")
-    print (response.text)
+def test_users_get(client):
+    """Test the users route."""
+    response = client.get('/users')
     assert response.status_code == 200
 
-def test_post_user():
-    payload = {"nome": "Teste"}
-    response = requests.post(f"{url}/users", json=payload)
-    print (response.text)
+def test_users_post(client):
+    """Test the users post route."""
+    response = client.post('/users', json={"nome": "Teste"})
     assert response.status_code == 201
