0d66e25e7360d91af0c0ab2ae745b4da5857d4df
Gabriel Carlos <gabrielcarlosoliveira0@gmail.com>
2025-11-06 18:09:50 -0300

    add new logging in app

--- src/app/hello.py
+++ src/app/hello.py
@@ -1,4 +1,8 @@
 from flask import Flask, jsonify, request
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -14,13 +18,16 @@ def create_user():
         nome = data.get("nome")
 
         if not nome:
+            app.logger.info("failed to login: nome is missing")
             return jsonify({ "error": "Nome is required" }), 400
         
         usuarios.append(nome)
+        app.logger.info(f"added to users {nome}")
         return jsonify ({
             "message": "Usuario Adicionado",
             "usuarios": usuarios
         }), 201
+        
     
 @app.route("/users", methods=["GET"])
 def get_users():
